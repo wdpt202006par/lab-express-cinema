0adc4c8de84ce658e1475bceca3f7bd1f15948bb
marwa.brj@gmail.com <marwa.brj@gmail.com>
2020-09-03 21:16:01 +0200

    marwa

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,19 @@ const router = express.Router();
 router.get('/', (req, res, next) => res.render('index'));
 
 
+router.get('/movies', (req, res, next) => {
+    Movie.find({})
+      .then((allMoviesFromDb) => {
+        //
+        res.render('movies-list', {
+          movies: allMoviesFromDb
+        })
+      })
+      .catch(err => {
+        console.log('💥', err)
+        next(err); // 
+      })
+  })
+
 
 module.exports = router;
