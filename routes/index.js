cf2d87b3bfefc26f9726c76371650814b43bdbf6
Gonzalo Cappa <goncappa@gmail.com>
2020-09-03 21:33:33 +0200

    1,2,3 not done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
+
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({})
+    .then((allMoviesFromDb) => {
+      //
+      res.render('movies', {
+        movies: allMoviesFromDb
+      })
+    })
+    .catch(err => {
+      console.log('💥', err)
+      next(err); // 
+    })
+})
 
 module.exports = router;
