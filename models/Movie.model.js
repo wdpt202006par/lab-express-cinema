f01d5936b401c7f3829f9ecbe7aa7e58b2243667
toufiq23 <drrameraaron023@gmail.com>
2020-09-03 20:54:35 +0200

    itiration 1 done

---
+++ models/Movie.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// destructuring
+// const { Schema } = mongoose; // destructuring
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+	title: String,
+	description: String,
+	author: String,
+	rating: Number,
+	// created_at: 
+  // updated_at:
+}, {
+	timestamps: true
+})
+
+const movieModel = mongoose.model('Movies', movieSchema);
+
+module.exports = movieModel;
