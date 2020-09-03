780d23229b53ff66025caf307aa13e872cc22e38
kardelen-karatas <kardelen.karatas@gmail.com>
2020-09-03 21:33:03 +0200

    iteration 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const Movie = require('../models/Movie.model') 
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then((allMovies) => {
+        // res.send(allMovies)
+        console.log(allMovies)
+        res.render('movies', {
+            movies:allMovies
+        })
+    })
+    .catch(err => {
+        console.log('ERROR while finding MOVIES data', err);
+        next(err);
+    })
+});
 
 module.exports = router;
