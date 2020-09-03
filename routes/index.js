81e2ff82fbea95a10ca811042f26f599afb3034e
Nina Quaresma <quaresma.nina@gmail.com>
2020-09-03 21:40:03 +0200

    Iteration 1

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
@@ -7,17 +8,17 @@ router.get('/', (req, res, next) => res.render('index'));
 
 router.get('/movies', (req, res, next) => {
     Movie.find({})
-      .then((allMoviesFromDb) => {
-        //
-        res.render('movies-list', {
-          movies: allMoviesFromDb
+        .then((allMoviesFromDb) => {
+            console.log('toto')
+            res.render('movies-list', {
+                movies: allMoviesFromDb
+            })
         })
-      })
-      .catch(err => {
-        console.log('💥', err)
-        next(err); // 
-      })
-  })
+        .catch(err => {
+            console.log('💥', err)
+            next(err); // 
+        })
+})
 
 
 module.exports = router;
